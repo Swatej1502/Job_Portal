db424483ac0e6d4ccadfc35ceb44f4593069a1a8
Swatej1502 <111179892+Swatej1502@users.noreply.github.com>
2024-03-02 01:08:13 +0530

    commited

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 # Job_Portal
+=======
+# jovian-careers-website
+A careers website for Jovian
+>>>>>>> 2cd0aa6 (commited)
---
+++ database.py
@@ -0,0 +1,42 @@
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+engine = create_engine(os.getenv("MY_KEY"))
+
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.mappings().all():
+      #print(row)
+      jobs.append(row)
+    return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text(f"SELECT * FROM jobs WHERE id = :val"),
+      {"val": id}
+    )
+    rows = result.mappings().all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
+
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+
+    conn.execute(query, 
+                 {"job_id":job_id,
+                 "full_name":data['full_name'],
+                 "email":data['email'],
+                 "linkedin_url":data['linkedin_url'],
+                 "education":data['education'],
+                 "work_experience":data['work_experience'],
+                 "resume_url":data['resume_url']})
